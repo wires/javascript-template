7a14aaa1f26265056b76a5231d434c74b5f64ed8
wires <jelle@defekt.nl>
2014-01-30 14:04:02 +0100

    live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,29 @@ var concat = require('gulp-concat');
 // npm bundle packing
 var browserify = require('gulp-browserify');
 
-// live reloading
+
+// start express static file server
+var express = require('express');
+var app = express();
+app.use(require('connect-livereload')());
+app.use(express.static(__dirname + '/dist'));
+app.listen(4000);
+
+// start livereload server
 var refresh = require('gulp-livereload');
 var server = require('tiny-lr')();
-
-gulp.task('server', function() {
-	// start express static file server
-	var app = require('express')();
-	app.use(require('connect-livereload')());
-	app.use(express.static(__dirname + '/dist'));
-	app.listen(4000);
-
-	// start livereload server
-	server.listen(35729, function(err) {
-		if(err) return console.log(err);
-	});
-
-	console.log("Live-reload server started at http://localhost:4000");
+server.listen(35729, function(err) {
+	if(err) return console.log(err);
 });
 
 function dest() {
 	return gulp.dest('./dist/');
 }
 
-gulp.task('default', function(){
-	gulp.run(['server', 'build']);
-});
 
-gulp.task('build', function(){
+console.log("Live-reload server started at http://localhost:4000");
+
+gulp.task('default', function(){
   gulp.src('./src/index.html')
     .pipe(dest())
 	.pipe(refresh(server));
@@ -50,5 +45,5 @@ gulp.task('build', function(){
 });
 
 gulp.watch('src/**', function(){
-  gulp.run('build');
+  gulp.run('default');
 });
